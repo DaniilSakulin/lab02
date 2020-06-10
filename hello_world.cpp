284a47be00126333d57ddfe27c8c04ad48134a9a
DaniilSakulin <sakulin_daniil@mail.ru>
2020-06-10 17:59:12 +0300

    Program with comments

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
+/*
+* Main function
+*/
 int main(){
+// reas user input
 std::string name;
 std::cin>>name;
+// write to console
 std::cout<<"Hello world from "<<name<<endl;
 return 0;
 }
