d25d2b06ed29b8816cab20e027ae58e5565db2d4
DaniilSakulin <sakulin_daniil@mail.ru>
2020-06-10 20:48:32 +0300

    REAL

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
+/*
+* Main function
+*/
 int main(){
+// read user input
 std::string name;
 std::cin>>name;
+// write to console
 std::cout<<"Hello world from "<<name<<endl;
 return 0;
 }
