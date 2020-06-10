27bdc577de47bd4692a8b786aabc4422358cbf35
DaniilSakulin <sakulin_daniil@mail.ru>
2020-06-10 20:23:52 +0300

    New style

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
-using namespase std;
 int main(){
-string name;
-cin>>name;
-cout<<"Hello world from "<<name<<endl;
+std::string name;
+std::cin>>name;
+std::cout<<"Hello world from "<<name<<endl;
 return 0;
 }
